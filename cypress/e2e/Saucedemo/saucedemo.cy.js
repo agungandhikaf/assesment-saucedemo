22362399d88f898490a22c72841775c20c1725dd
agungandhikaf <agungandhikaf@gmail.com>
2024-03-29 00:23:57 +0700

    perubahan perulangan

--- cypress/e2e/Saucedemo/saucedemo.cy.js
+++ cypress/e2e/Saucedemo/saucedemo.cy.js
@@ -1,24 +1,41 @@
 import Login from "../../support/page-object/login-page";
 import Verify from "../../support/page-object/verify-components";
 import Logout from "../../support/page-object/logout-page";
+import data from "../../fixtures/credentials.json";
 
-const userData = require('../../fixtures/credentials')
+// const userData = require('../../fixtures/credentials')
 const login = new Login()
 const logout = new Logout()
 const verify = new Verify()
 
 describe('', () => {
-    before(() => {
+    beforeEach(() => {
         login.goToPage()
         verify.urlPage()
     });
-    it('', () => {
-        for (let i=0; i < userData.length; i++){
-            login.inputUsername(userData[i].username)
-            login.inputPassword(userData[i].password)
+
+    data.forEach(element => {
+        it('Login with ' + element.expected, () => {
+            login.inputUsername(element.username)
+            login.inputPassword(element.password)
             login.clickLogin()
             logout.doLogout()
-            login.goToPage()
-        }
+            
+        });
     });
+    // it('', () => {
+    //     for (let i=0; i < userData.length; i++){
+    //         login.inputUsername(userData[i].username)
+    //         login.inputPassword(userData[i].password)
+    //         login.clickLogin()
+    //         if (){
+
+    //         }
+    //         // if (verify.errorMessage('locked out')){
+    //         //     continue;
+    //         // }
+    //         // login.inputUsername().clear()
+    //         // login.inputPassword().clear()
+    //     }
+    // });
 });
--- cypress/support/page-object/verify-components.js
+++ cypress/support/page-object/verify-components.js
@@ -5,17 +5,8 @@ class Verify{
     urlPage(){
         cy.url().should('eq', Cypress.env('BASE_URL'))
     }
-    // failedAlert(message){
-    //     cy.get('[data-layer="Content"]').should('have.text', message)
-    // }
-    // snackbarAlert(message){
-    //     cy.wait(700)
-    //     cy.get('.ant-notification-notice-description').should('have.text', message)
-    //     cy.get('[class="ant-notification-notice-close-x"]').click()
-    // }
-    // clickSubmit(text){
-    //     cy.wait(300)
-    //     cy.get('[type="submit"]').should('have.text', text).click()
-    // }
+    errorMessage(word){
+        cy.xpath("//h3[@data-test='error']").contain(word)
+    }
 }
 export default Verify
